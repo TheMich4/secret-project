57d57f2905fa1a57e1eab7bc3d25d858de4f3f67
Michał Dyczkowski <themich4@gmail.com>
2020-03-21 07:33:03 -0700

    Updates

---
+++ precache-manifest.3657968f1dfe4dc4ee0009bcf4c11a82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ccbcff22d4c2696e2c5a411c57ef55",
+    "url": "/secret-project/index.html"
+  },
+  {
+    "revision": "1d1f57e448f3f397559b",
+    "url": "/secret-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecc8439364d92b6625ba",
+    "url": "/secret-project/static/js/2.3c5bd8b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/secret-project/static/js/2.3c5bd8b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1f57e448f3f397559b",
+    "url": "/secret-project/static/js/main.a33c04b8.chunk.js"
+  },
+  {
+    "revision": "96855143db598054c11d",
+    "url": "/secret-project/static/js/runtime-main.528cb816.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/secret-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
